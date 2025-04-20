956128210cc1e2aa87be5a92abc5287e95fb0758
soyoonL <diamomo777@naver.com>
2025-04-20 22:44:38 +0900

    [Feat] 제출

---
+++ Assets/Scripts/EnemyTraceController1.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTraceController1 : MonoBehaviour
+{
+    public float moveSpeed = .5f;
+    public float raycastDistance = .2f;
+    public float traceDistance = 2f;
+
+    private Transform player;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform; 
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Vector2 direction = player.position - transform.position;
+
+        if (direction.magnitude > traceDistance)
+            return;
+
+        Vector2 directionNormalized=direction.normalized;
+
+        RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position, directionNormalized, raycastDistance);
+        Debug.DrawRay(transform.position, directionNormalized * raycastDistance, Color.red);
+
+        foreach (RaycastHit2D rhit in hits)
+        {
+            if (rhit.collider != null && rhit.collider.CompareTag("Obstacle"))
+            {
+                Vector3 alternativeDirection = Quaternion.Euler(0f, 0f, -90f) * direction;
+                transform.Translate(alternativeDirection * moveSpeed * Time.deltaTime);
+            }
+            else
+            {
+                transform.Translate(direction * moveSpeed * Time.deltaTime);
+            }
+        }
+    }
+}
